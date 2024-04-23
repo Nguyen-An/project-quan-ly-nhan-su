92e5b29070d1ba6bcb10b0df376916486bb13cca
An Nguyen <vanan6b@gmail.com>
2024-04-23 23:30:58 +0700

    update code

--- src/app/component/report/report-salary/report-salary.component.ts
+++ src/app/component/report/report-salary/report-salary.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, NonNullableFormBuilder } from '@angular/forms';
 import { NzModalRef } from 'ng-zorro-antd/modal';
-import { MODE_FORM } from 'src/app/const/common';
+import { MODE_FORM, formatCurrency } from 'src/app/const/common';
 import { POSITIONS, SALARY, STATUS } from 'src/app/const/initValue';
-import { Account, Employee, accountFakeData, employeeFakeData } from 'src/app/const/model';
+import { Account, Employee, SalartEmployee, accountFakeData, employeeFakeData, salartEmployee } from 'src/app/const/model';
+import * as XLSX from 'xlsx';
 
 @Component({
   selector: 'app-report-salary',
@@ -39,13 +40,30 @@ export class ReportSalaryComponent {
   MODE_FORM = MODE_FORM;
   STATUS = STATUS;
   SALARY = SALARY;
-  listOfData: Employee[] = [];
+  listOfData: SalartEmployee[] = [];
+  listFiter: any[] = [];
   total = 0;
   currentPage = 1;
   size = 10;
 
   ngOnInit(): void {
-    this.listOfData = employeeFakeData.map(employee => {
+    this.getData('all')
+  }
+
+  getData(key: any) {
+    console.log('key', key);
+    
+    if (key == 'all') {
+      this.listFiter = salartEmployee;
+    } else if (key == '0-6') {
+      this.listFiter = salartEmployee.filter((item: any) => Number(item.salary) < 6000000);
+    } else if (key == '6-9') {
+      this.listFiter = salartEmployee.filter((item: any) => (Number(item.salary) >= 6000000 && Number(item.salary) <= 9000000));
+    } else if (key == '9-m') {
+      this.listFiter = salartEmployee.filter((item: any) => Number(item.salary) > 9000000);
+    }
+
+    this.listOfData = this.listFiter.map((employee: any) => {
       let positionValue = POSITIONS.find(position => position.key == employee.position)?.value
       employee.positionView = positionValue ? positionValue : '--';
 
@@ -55,14 +73,56 @@ export class ReportSalaryComponent {
       return employee;
     });
 
-    this.total = employeeFakeData.length
+    this.total = this.listFiter.length
+
+    this.listOfData = this.getPaginatedData(this.currentPage, this.size);
+  }
+
+  getPaginatedData(currentPage: number, size: number): SalartEmployee[] {
+    const startIndex = (currentPage - 1) * size;
+    const endIndex = startIndex + size;
+    return this.listFiter.slice(startIndex, endIndex);
   }
 
   handlePageChange(pageTarget: any) {
-    console.log('pageTarget: ', pageTarget);
+    this.currentPage = pageTarget;
+    this.listOfData = this.getPaginatedData(this.currentPage, this.size);
+  }
+
+  formatCurrency(value: string) {
+    return formatCurrency(value);
+  }
+
+  loadDataFile() {
+    this.getData(this.validateForm.value.searchCtrl);
   }
 
   export() {
+    let dataExport: any[] = [];
+
+    this.listFiter.forEach((item, i) => {
+      let dataItem = {
+        'STT': i+1,
+        'Tên nhân viên': item.userName,
+        'Mã nhân viên': item.userCode + (i+1),
+        'Email': item.email,
+        'Chức vụ': item.positionView,
+        'Số điện thoại': item.phone,
+        'Địa chỉ': item.address,
+        'Trạng thái': item.statusView,
+        'Mức lương': formatCurrency(item.salary),
+      }
+      dataExport.push(dataItem);
+    });
     
+    // Tạo Worksheet
+    const ws: XLSX.WorkSheet = XLSX.utils.json_to_sheet(dataExport);
+
+    // Tạo Workbook và thêm Worksheet vào đó
+    const wb: XLSX.WorkBook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, 'Data');
+
+    /* save to file */
+    XLSX.writeFile(wb, 'Danh sách lương nhân viên theo mức lương.xlsx');
   }
 }
